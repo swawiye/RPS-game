c727f177e56e6bccdb17ad3a64050ff213b55c25
swawiye <sovereign.wawiye@gmail.com>
2025-04-12 22:59:15 +0300

    button functionality

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ let computerMove = '';
 function pickComputerMove() {
     const randomNumber = Math.random();
 
-    //Rock
     if (randomNumber >= 0 && randomNumber < 1/3) {
         computerMove = 'rock';
     } else if (randomNumber >= 1/3 && randomNumber < 2/3) {
@@ -11,36 +10,6 @@ function pickComputerMove() {
     } else if (randomNumber >= 2/3 && randomNumber < 1) {
         computerMove = 'scissors';
     };
-    
-    let result = '';
-    if (computerMove === 'rock') {
-        result = 'It is a tie.';
-    } else if (computerMove === 'paper') {
-        result = 'You lose.';
-    } else if (computerMove === 'scissors') {
-        result = 'You win!';
-    };
-    
-    alert(`You picked rock. Computer picked ${computerMove}. ${result}`);
-
-    //Paper
-    if (randomNumber >= 0 && randomNumber < 1/3) {
-        computerMove = 'rock';
-    } else if (randomNumber >= 1/3 && randomNumber < 2/3) {
-        computerMove = 'paper';
-    } else if (randomNumber >= 2/3 && randomNumber < 1) {
-        computerMove = 'scissors';
-    };
-
-    if (computerMove === 'paper') {
-        result = 'It is a tie.';
-    } else if (computerMove === 'scissors') {
-        result = 'You lose.';
-    } else if (computerMove === 'rock') {
-        result = 'You win!';
-    };
-
-    alert(`You picked paper. Computer picked ${computerMove}. ${result}`)
 };
 
 pickComputerMove();
